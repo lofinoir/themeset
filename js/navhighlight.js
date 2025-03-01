b4600af5633293b345d54625476536bf4634cb0b
lofinoir <112433821+lofinoir@users.noreply.github.com>
2025-02-28 23:04:57 -0800

    Add files via upload

---
+++ js/navhighlight.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".nav-link");
+  const currentPath = window.location.pathname;
+
+  links.forEach((link) => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("active");
+    }
+  });
+});
