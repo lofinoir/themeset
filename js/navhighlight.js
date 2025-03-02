dff989aa921ca5e5dc233491b8cd8da2f07da4ed
lofinoir <112433821+lofinoir@users.noreply.github.com>
2025-03-01 20:13:00 -0800

    Update navhighlight.js
    changed from relative path to full url

--- js/navhighlight.js
+++ js/navhighlight.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".nav-link");
-  const currentPath = window.location.pathname;
+    const links = document.querySelectorAll(".nav-link");
+    const currentURL = window.location.href; // Get full URL
 
-  links.forEach((link) => {
-    if (link.getAttribute("href") === currentPath) {
-      link.classList.add("active");
-    }
-  });
+    links.forEach((link) => {
+        if (link.href === currentURL) {
+            link.classList.add("active");
+        }
+    });
 });
